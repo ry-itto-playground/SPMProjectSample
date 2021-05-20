fd3e5b3c21ff912f6edd0b7a911d643cd205cb96
ryoya ito <110ryoya@gmail.com>
2021-05-20 22:55:04 +0900

    fix to show jpg, pdf images

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SampleAppPackage",
+    defaultLocalization: "ja",
     platforms: [
         .iOS(.v14),
         .macOS(.v10_12),
@@ -30,7 +31,8 @@ let package = Package(
 //                .target(name: "VueFlux"),
             ],
             resources: [
-                .process("Resources/"),
+                // Resources 内で 画像を置いている場合、なぜか png しか　Image や　UIImage で参照する際に画像として認識してくれない
+//                .process("Resources/"),
             ]),
         .testTarget(
             name: "SampleAppPackageTests",
--- Sources/SampleAppPackage/SampleView.swift
+++ Sources/SampleAppPackage/SampleView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 public struct SampleView: View {
 
-    public init() {}
+    public init() {
+        print(Bundle.module.path(forResource: "image_jpg", ofType: "jpg"))
+    }
     public var body: some View {
         VStack {
 //            Image(uiImage: UIImage(named: "image_jpg", in: Bundle.module, with: nil)!)
-            Image(uiImage: UIImage(named: "image_png", in: Bundle.module, with: nil)!)
+            
+            Image("image_pdf", bundle: .module)
+//            Image(uiImage: UIImage(named: "image_png", in: Bundle.module, with: nil)!)
         }
     }
 }
